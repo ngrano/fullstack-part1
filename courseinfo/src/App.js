f0fdec8292d9270ca69e4d6d6982dac3c146a276
Niklas Granö <niklas.grano@gmail.com>
2022-09-09 19:22:09 +0300

    Exercise 1.3 done

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,11 +1,17 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = () => {
     return (
@@ -17,7 +23,7 @@ const App = () => {
     return (
       <div>
         <p>
-        {props.part} {props.exercises}
+        {props.part.name} {props.part.exercises}
         </p>
       </div>
       )
@@ -26,15 +32,15 @@ const App = () => {
   const Content = () => {
     return (
       <div>
-        <Part part={part1} exercises={exercises1} />
-        <Part part={part2} exercises={exercises2} />
-        <Part part={part3} exercises={exercises3} />
+        <Part part={part1} />
+        <Part part={part2} />
+        <Part part={part3} />
       </div>
     )
   }
 
   const Total = () => {
-    return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    return <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
   }
 
   return (
